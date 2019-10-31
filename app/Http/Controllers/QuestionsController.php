f6c5df8dc28660427a4f7481a11120094d06935f
abhaygolflan <abhay.gupta@golflan.com>
2019-10-31 22:38:15 +0530

    Build Question Form

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -35,7 +35,9 @@ class QuestionsController extends Controller
      */
     public function create()
     {
-        //
+        $question = new Question();
+
+        return view('questions.create', compact('question'));
     }
 
     /**
