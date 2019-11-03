9a7991b4f69fc8a7ce98cc067bc07207c3a23682
abhaygolflan <abhay.gupta@golflan.com>
2019-11-04 00:15:03 +0530

    First part of vote control system

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -3,34 +3,63 @@
 @section('content')
 
 <div class="container">
- 
+
     <div class="row mt-4">
-    <div class="col-md-12">
+        <div class="col-md-12">
 
-<div class="card">
+            <div class="card">
 
-<div class="card-header">
+                <div class="card-title">
 
-<div class="d-flex align-itmes-center">
+                    <div class="d-flex align-itmes-center">
 
-    <h2></h2>
-    <div class="ml-auto"><a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">Back to all Questions</a></div>
+                        <h2></h2>
+                        <div class="ml-auto"><a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">Back to all Questions</a></div>
 
-</div>
+                    </div>
 
-</div>
+                </div>
 
-    <div class="card-body">
-    <div class="card_title">
+                <div class="card-body">
+                    <div class="card_title">
 
-<h2>{{ $question->title }}</h2>
-</div>
-</div>
-</div>
-</div>
+                        <h2>{{ $question->title }}</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
 
 
     </div>
+
+    <div class="media">
+        <div class="d-fex flex-colum vote-controls">
+
+            <a title="This question is useful" class="vote-up">
+
+                Vote up
+
+            </a>
+
+            <span class="votes-count">1230</span>
+
+            <a title="This question is not useful" class="vote-down off">
+
+                Vote down
+
+            </a>
+
+            <a title="click to mark as favorite question (Click again to undo)" class="favorite">
+
+                Favorite
+
+                <span class="favorites-count">123</span>
+
+            </a>
+
+        </div>
+    </div>
+
 
     <div class="row mt-4">
 
